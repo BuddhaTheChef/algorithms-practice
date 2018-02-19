d5fb57d546585a33501863f38a103ea98e891a98
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-19 12:12:42 -1000

    MaxChar

--- app.js
+++ app.js
@@ -44,13 +44,42 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //                                 reversed integer
 
-function reverseInt(n) {
+// function reverseInt(n) {
+//
+//   const reversed = n.toString().split('').reverse().join('');
+//   //math sign returns numbeer if it is negative or positive
+//   return parseInt(reversed) * Math.sign(n);
+// }
 
-  const reversed = n.toString().split('').reverse().join('');
-  //math sign returns numbeer if it is negative or positive
-  return parseInt(reversed) * Math.sign(n);
+///////////////////////////////////////////////////////////////////////////////////////////////
+//                             letter most used in a string
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+  for(let char of str) {
+    if(charMap[char]) {
+      charMap[char]++;
+    }
+    else {
+      charMap[char] = 1;
+    }
+  }
+  for(let char in charMap) {
+    if(charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
+//                                       FizzBuzz
 
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////
