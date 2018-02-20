f951d4df8efb9ad7efa7fe918c09475eb019e696
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-20 12:59:06 -1000

    steps

--- app.js
+++ app.js
@@ -194,5 +194,46 @@ function capitalize(str) {
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
+//                                      Steps
+
+//  Solution 1
+function steps(n) {
+  for(let row = 0; row < n; row++) {
+    let stair = '';
+    for(let column = 0; column < n; column++) {
+      if(column <= row) {
+        stair += '#';
+      }
+      else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
+
+//  Solution 2
+
+function steps(n, row = 0, stair = '') {
+  if(n === row) {
+    return;
+  }
+
+  if(n === stair.length) {
+    console.log(stair);
+    steps(n, row + 1);
+    return
+  }
+
+  if(stair.length <= row) {
+    stair += '#';
+  }
+  else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
+
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
