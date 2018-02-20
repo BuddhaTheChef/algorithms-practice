cecfa75c23f35241b9e9ceab28d1a0cfb93910b4
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-20 13:35:40 -1000

    vowels

--- app.js
+++ app.js
@@ -281,6 +281,28 @@ function pyramid(n, row = 0, level = '') {
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //                                  VOWELS
 
+//  Solution 1
+
+function vowels(str) {
+  let count = 0;
+  const check = ['a','e','i','o','u'];
+
+  for(let char of str.toLowerCase()) {
+    if(check.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+//  Solution 2
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
