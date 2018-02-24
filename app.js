6ec45ec4bde1fadfe2ad68bf0b22ba795f9581d5
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-23 14:41:19 -1000

    Three examples using linkedList

--- app.js
+++ app.js
@@ -672,7 +672,21 @@ function circular(list) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
-// 
+//                           From last linkedList examples
 
+function fromLast(list, n) {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while(n > 0) {
+    fast = fast.next;
+    n--;
+  }
+  while(fast.next) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
