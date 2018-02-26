55776e0cdaf28cfa23b1c8a2268bca3f42df9c7c
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-26 12:14:18 -1000

    bf traversal example

--- app.js
+++ app.js
@@ -735,6 +735,25 @@ class Tree {
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
+//                                example of BF traversal 
 
+function levelWidth(root) {
+  const counters = [0];
+  const arr = [root,'s'];
+
+  while(arr.length > 1) {
+    const node = arr.shift();
+
+    if(node === 's') {
+      counters.push(0);
+      arr.push('s');
+    }
+    else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
