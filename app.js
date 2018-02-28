f6d17fb357d0e5c7067e031b7d20fe3327876526
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-27 15:27:14 -1000

    events library

--- app.js
+++ app.js
@@ -826,5 +826,21 @@ class Events {
       this.events[eventName] = [callback];
     }
   }
+  // Trigger all callbacks associated
+// with a given eventName
+trigger(eventName) {
+  if(this.events[eventName]) {
+    for(let cb of this.events[eventName]) {
+      cb();
+    }
+  }
+}
+
+// Remove all event handlers associated
+// with the given eventName
+off(eventName) {
+  delete this.events[eventName];
+}
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
